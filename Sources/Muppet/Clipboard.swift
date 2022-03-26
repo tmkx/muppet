42b52497aae1e914c31a43d4eeab863d37ef68a8
Tmk <i@tmk.im>
2022-03-26 21:44:34 +0800

    Add mouse utils

--- Sources/Muppet/Clipboard.swift
+++ Sources/Muppet/Clipboard.swift
@@ -2,16 +2,26 @@ import Cocoa
 
 /// Clipboard Utils
 public struct Clipboard {
+    /// Clear clipboard
+    public static func clear() {
+        NSPasteboard.general.clearContents()
+    }
+
     /// Read clipboard text
     public static func readText() -> String? {
         NSPasteboard.general.string(forType: .string)
     }
 
     /// Write text to clipboard
-    public static func writeText(_ text: String?) -> Bool {
-        NSPasteboard.general.clearContents()
-        if let text = text {
-            return NSPasteboard.general.setString(text, forType: .string)
+    public static func write(text: String) -> Bool {
+        clear()
+        return NSPasteboard.general.setString(text, forType: .string)
+    }
+
+    /// Whether clipboard is empty
+    public static func isEmpty() -> Bool {
+        if let count = NSPasteboard.general.pasteboardItems?.count {
+            return count == 0
         } else {
             return true
         }
---
+++ Sources/Muppet/Mouse.swift
@@ -0,0 +1,75 @@
+import Cocoa
+
+/// Mouse Utils
+///
+/// You can get mouse position by pressing `⌘ + ⇧ + 4`
+public struct Mouse {
+    /// Get mouse position
+    public static func getPosition() -> CGPoint {
+        let location = NSEvent.mouseLocation
+        return CGPoint(x: location.x, y: location.y)
+    }
+
+    /// Move mouse
+    public static func moveTo(point: CGPoint) {
+        let eventMove = CGEvent(mouseEventSource: nil, mouseType: .mouseMoved, mouseCursorPosition: point, mouseButton: .left)
+        eventMove?.post(tap: .cghidEventTap)
+    }
+
+    /// Get button type. (down, up, dragged)
+    static func getButtonType(_ button: CGMouseButton) -> (CGEventType, CGEventType, CGEventType) {
+        switch button {
+        case .right:
+            return (.rightMouseDown, .rightMouseUp, .rightMouseDragged)
+        case .center:
+            return (.otherMouseDown, .otherMouseUp, .otherMouseDragged)
+        default:
+            return (.leftMouseDown, .leftMouseUp, .leftMouseDragged)
+        }
+    }
+
+    /// Click
+    public static func click(at point: CGPoint, using button: CGMouseButton = .left) {
+        let (mouseDownType, mouseUpType, _) = getButtonType(button)
+
+        let eventDown = CGEvent(mouseEventSource: nil, mouseType: mouseDownType, mouseCursorPosition: point, mouseButton: button)
+        let eventUp = CGEvent(mouseEventSource: nil, mouseType: mouseUpType, mouseCursorPosition: point, mouseButton: button)
+
+        eventDown?.post(tap: .cghidEventTap)
+        usleep(10_000)
+        eventUp?.post(tap: .cghidEventTap)
+    }
+
+    /// Double Click
+    public static func dbclick(at point: CGPoint, using button: CGMouseButton = .left) {
+        let (mouseDownType, mouseUpType, _) = getButtonType(button)
+
+        let downEvent = CGEvent(mouseEventSource: nil, mouseType: mouseDownType, mouseCursorPosition: point, mouseButton: button)
+        let upEvent = CGEvent(mouseEventSource: nil, mouseType: mouseUpType, mouseCursorPosition: point, mouseButton: button)
+
+        downEvent?.setIntegerValueField(.mouseEventClickState, value: 1)
+        downEvent?.post(tap: .cghidEventTap)
+        upEvent?.setIntegerValueField(.mouseEventClickState, value: 1)
+        upEvent?.post(tap: .cghidEventTap)
+
+        downEvent?.setIntegerValueField(.mouseEventClickState, value: 2)
+        downEvent?.post(tap: .cghidEventTap)
+        upEvent?.setIntegerValueField(.mouseEventClickState, value: 2)
+        upEvent?.post(tap: .cghidEventTap)
+    }
+
+    /// Drag
+    public static func drag(from: CGPoint, to: CGPoint, using button: CGMouseButton = .left) {
+        let (mouseDownType, mouseUpType, mouseDragType) = getButtonType(button)
+
+        let downEvent = CGEvent(mouseEventSource: nil, mouseType: mouseDownType, mouseCursorPosition: from, mouseButton: button)
+        let dragEvent = CGEvent(mouseEventSource: nil, mouseType: mouseDragType, mouseCursorPosition: to, mouseButton: button)
+        let upEvent = CGEvent(mouseEventSource: nil, mouseType: mouseUpType, mouseCursorPosition: to, mouseButton: button)
+
+        downEvent?.post(tap: .cghidEventTap)
+        usleep(100_000)
+        dragEvent?.post(tap: .cghidEventTap)
+        usleep(100_000)
+        upEvent?.post(tap: .cghidEventTap)
+    }
+}
